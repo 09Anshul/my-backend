01da0fa45d013b340bfd8be26cf5f0aef04134e4
09Anshul <115798975+09Anshul@users.noreply.github.com>
2025-04-24 14:50:27 +0530

    Update chatRoutes.js

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { chatWithAI } from "../controllers/chatController.js";
+import  chatWithAI  from "../controllers/chatController.js";
 
 const router = express.Router();
 
